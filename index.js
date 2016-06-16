bbc98b2c022ac650b3b55660c6d8f767d7540c50
Alexander Corn <mckay@doctormckay.com>
2016-06-16 15:47:41 -0400

    Added encoding

--- index.js
+++ index.js
@@ -6,24 +6,29 @@ if (typeof dcodeIO !== 'undefined') {
 	ByteBuffer = require('bytebuffer');
 }
 
+const MAX_UINT8 = 255;
+const MAX_UINT16 = 65535;
+const MAX_UINT32 = 4294967295;
+
 var DataType = {
 	"End": 0x00, // end of an object or array
 	"Object": 0x01, // read named values (names prefixed to value as varint-prefixed UTF-8 string) until you hit End
 	"Array": 0x02, // read ordered values until you hit End
 	"String": 0x03, // varint-prefixed UTF-8 string
-	"Boolean": 0x04, // encoded as byte 0/1
-	"Null": 0x05, // no actual data follows
-	"PositiveVarInt64": 0x06, // LET THE NUMBER TYPES BEGIN!
-	"NegativeVarInt64": 0x07,
-	"PositiveInt8": 0x08, // Use Int8 if 0 <= abs(x) < 128. Max uint8 = 255
-	"NegativeInt8": 0x09,
-	"PositiveInt16": 0x0A, // Use Int16 if 128 <= abs(x) < 16384. Max uint16 = 65535
-	"NegativeInt16": 0x0B,
-	"PositiveInt32": 0x0C, // Use Int32 if 16384 <= abs(x) < 268435456. Max uint32 = 4294967295
-	"NegativeInt32": 0x0D,
-	"PositiveInt64": 0x0E, // Use Int64 if abs(x) >= 36028797018963970 (approximately). Use varint64 if 4294967296 <= abs(x) < 36028797018963970
-	"NegativeInt64": 0x0F,
-	"Double": 0x10
+	"BooleanFalse": 0x04, // no sense in wasting a data byte for 1/0
+	"BooleanTrue": 0x05,
+	"Null": 0x06, // no actual data follows
+	"PositiveVarInt64": 0x07, // LET THE NUMBER TYPES BEGIN!
+	"NegativeVarInt64": 0x08,
+	"PositiveInt8": 0x09,
+	"NegativeInt8": 0x0A,
+	"PositiveInt16": 0x0B,
+	"NegativeInt16": 0x0C,
+	"PositiveInt32": 0x0D,
+	"NegativeInt32": 0x0E,
+	"PositiveInt64": 0x0F, // Use Int64 if abs(x) >= 36028797018963970 (approximately). Use varint64 if 4294967296 <= abs(x) < 36028797018963970
+	"NegativeInt64": 0x10,
+	"Double": 0x11
 };
 
 exports.decode = function(input) {
@@ -65,8 +70,12 @@ exports.decode = function(input) {
 				data = buffer.readVString();
 				break;
 
-			case DataType.Boolean:
-				data = !!buffer.readUint8();
+			case DataType.BooleanFalse:
+				data = false;
+				break;
+
+			case DataType.BooleanTrue:
+				data = true;
 				break;
 
 			case DataType.Null:
@@ -113,5 +122,91 @@ exports.decode = function(input) {
 		}
 
 		return data;
+	}
+};
+
+exports.encode = function(input) {
+	var buffer = new ByteBuffer(128, false);
+	return writeValue(input).flip().toBuffer();
+
+	function writeValue(value, name) {
+		var jsType = typeof value;
+		if (typeof value === 'string' && value.match(/^-?[0-9]+$/)) {
+			jsType = 'number';
+			if (value <= MAX_UINT32) {
+				value = parseInt(value, 10);
+			}
+		}
+
+		switch (jsType) {
+			case 'boolean':
+				writeType(value ? DataType.BooleanTrue : DataType.BooleanFalse, name);
+				break;
+
+			case 'string':
+				writeType(DataType.String, name, value).writeVString(value);
+				break;
+
+			case 'number':
+				if (isNaN(value)) {
+					throw new Error("Cannot encode NaN into binary JSON.");
+				}
+
+				if (value % 1) {
+					// This is a double
+					writeType(DataType.Double, name).writeDouble(value);
+				} else {
+					// This is an int. Figure out the most efficient encoding type.
+					var isNegative = value < 0;
+					if (isNegative) {
+						value *= -1; // positiveize it
+					}
+
+					var type = DataType.PositiveInt64;
+					var func = buffer.writeUint64;
+					if (value < MAX_UINT8) {
+						type = DataType.PositiveInt8;
+						func = buffer.writeUint8;
+					} else if (value < MAX_UINT16) {
+						type = DataType.PositiveInt16;
+						func = buffer.writeUint16;
+					} else if (value < MAX_UINT32) {
+						type = DataType.PositiveInt32;
+						func = buffer.writeUint32;
+					} else if (ByteBuffer.calculateVarint64(value) < 8) {
+						type = DataType.PositiveVarInt64;
+						func = buffer.writeVarint64;
+					}
+
+					if (isNegative) {
+						++type;
+					}
+
+					writeType(type, name);
+					func.call(buffer, value);
+				}
+
+				break;
+		}
+
+		return buffer;
+	}
+
+	function writeType(type, name, value) {
+		var size = (type == DataType.String ? ByteBuffer.calculateVarint64(value.length) + value.length : 32);
+		if (name) {
+			size += ByteBuffer.calculateVarint64(name.length) + name.length;
+		}
+
+		if (buffer.remaining() < size) {
+			buffer.resize(buffer.limit + Math.max(size, 128));
+		}
+
+		buffer.writeUint8(type);
+		if (name) {
+			buffer.writeVString(name);
+		}
+
+		return buffer;
 	}
 };
